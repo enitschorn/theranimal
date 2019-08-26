864382e1bba195cd5f2da432c5e6a97ffe7b2992
gimo ke <gke.acc@gmail.com>
2019-08-26 18:03:23 +1000

    add_update

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -20,6 +20,15 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(params[:animal])
+  end
+
   private
 
   def animal_params
