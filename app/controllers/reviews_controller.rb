77798a7ee53096a218f9e2d5d392ead4784b8033
Christine Schorn <cschorn91@gmail.com>
2019-08-27 17:37:00 +1000

    added validations and references to reviewscontroller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,6 @@
 class ReviewsController < ApplicationController
+  belongs_to :animal
+
+  validates :rating, presence: true, inclusion: { in: (0..5) }
+  validates :description, presence: true
 end
