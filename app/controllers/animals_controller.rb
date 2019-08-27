ac1e65e6136a7e82d23bd789845b773fe254aa27
gimo ke <gke.acc@gmail.com>
2019-08-27 11:07:55 +1000

    destroy added

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -29,6 +29,12 @@ class AnimalsController < ApplicationController
     @animal.update(animal_params)
   end
 
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animals_path
+  end
+
   private
 
   def animal_params
