c65be15aae6dbc8813908715fa3b7d86ec256634
Christine Schorn <cschorn91@gmail.com>
2019-08-27 15:07:54 +1000

    created review model and added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_055520) do
+ActiveRecord::Schema.define(version: 2019_08_27_050628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2019_08_26_055520) do
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.integer "rating"
+    t.bigint "animal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["animal_id"], name: "index_reviews_on_animal_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +49,5 @@ ActiveRecord::Schema.define(version: 2019_08_26_055520) do
   end
 
   add_foreign_key "animals", "users"
+  add_foreign_key "reviews", "animals"
 end
